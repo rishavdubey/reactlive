345db43f8e27dabffbf7f61f0dcb38b410b98891
rishavdubey <55137788+rishavdubey@users.noreply.github.com>
2022-06-04 01:29:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Resturant from './component/Basics/Resturant'
+// import UseState from './component/Hooks/useState'
+// import UseEffect from './component/Hooks/useEffect'
+// import UseReducer from './component/Hooks/useReducer'
+// import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+
+const App = () => {
+  return (
+    <>
+      {/* <Todo/> */}
+      {/* <Resturant/> */}
+      <Temp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/weather/temp.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import Weathercard from './weathercard';
+import './style.css'
+
+// https://api.openweathermap.org/data/2.5/weather?q=pune&appid=04306714592cbbd44b73b9dde0a12cea
+
+const Temp = () => {
+
+    const [searchValue, setSearchValue] = useState("pune");
+    const [tempInfo,setTempInfo]=useState({})
+
+    const getWeatherInfo = async() => {
+        try {
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=04306714592cbbd44b73b9dde0a12cea`;
+            
+            const res=await fetch(url);
+            const data=await res.json();
+            
+            const {temp,humidity,pressure}=data.main;
+            const {main:weathermood}=data.weather[0];
+            const {name}=data;
+            const {speed}=data.wind;
+            const {country,sunset}=data.sys;
+
+            const myNewWeatherInfo={
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+
+            setTempInfo(myNewWeatherInfo);
+            
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+    }
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []);
+    
+
+    return (
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input type="search"
+                        placeholder='search...'
+                        autoFocus
+                        id='serch'
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        className='searchTerm' />
+                    <button className="searchButton" type="button" onClick={getWeatherInfo}>Search</button>
+                </div>
+            </div>
+
+            {/* our temprature card */}
+            <Weathercard tempInfo={tempInfo}/>
+          
+
+        </>
+    )
+}
+
+export default Temp
